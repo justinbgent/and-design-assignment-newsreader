f93c342ced094a79042b3c6458e69b71dfb6abc2
Justin Gent <justinbgent@gmail.com>
2019-08-14 18:36:47 -0600

    Add news items to listActivity.

--- WireframeProject/app/src/main/java/com/example/wireframeproject/NewsListAdapter.kt
+++ WireframeProject/app/src/main/java/com/example/wireframeproject/NewsListAdapter.kt
@@ -3,11 +3,13 @@ package com.example.wireframeproject
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.wireframeproject.model.Data
 import kotlinx.android.synthetic.main.news_item.view.*
 
-class NewsListAdapter(val data: MutableList<Data>) : RecyclerView.Adapter<NewsListAdapter.ViewHolder>(){
+class NewsListAdapter(private val data: MutableList<Data>) : RecyclerView.Adapter<NewsListAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val viewGroup = LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
         return ViewHolder(viewGroup)
@@ -18,14 +20,14 @@ class NewsListAdapter(val data: MutableList<Data>) : RecyclerView.Adapter<NewsLi
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.news_title.text = data[position].newsTitle
-        holder.news_body.text = data[position].newsBody
-        holder.news_picture.setImageDrawable(data[position].image)
+        holder.newsTitle.text = data[position].newsTitle
+        holder.newsBody.text = data[position].newsBody
+        holder.newPicture.setImageDrawable(data[position].image)
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view){
-        val news_title = view.title_text
-        val news_picture = view.image_view
-        val news_body = view.body_text
+        val newsTitle: TextView = view.title_text
+        val newPicture: ImageView = view.image_view
+        val newsBody: TextView = view.body_text
     }
 }
--- WireframeProject/app/src/main/java/com/example/wireframeproject/ui/ListActivity.kt
+++ WireframeProject/app/src/main/java/com/example/wireframeproject/ui/ListActivity.kt
@@ -3,6 +3,7 @@ package com.example.wireframeproject.ui
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -19,10 +20,21 @@ class ListActivity : AppCompatActivity() {
 
         var newsArticles = mutableListOf<Data>()
 
-        newsArticles.add(Data(R.drawable.abstract_abstract_expressionism_art_2505693 as Drawable, "Flowers Gone Crazy", "..."))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.abstract_abstract_expressionism_art_2505693)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.aerial_photography_aerial_shot_aerial_view_2583847)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.adventure_automobile_classic_2533092)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.afterglow_beach_cliff_2555285)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.alley_architecture_buildings_2526517)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.architectural_design_architecture_bridge_2540653)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.beautiful_breathtaking_canada_day_2526105)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.bloom_blossom_flora_2567011)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.close_up_colorful_colors_2529148)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.clouds_coconut_trees_daylight_2486168)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.colorful_colourful_houses_2501965)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
+        newsArticles.add(Data(ContextCompat.getDrawable(this, R.drawable.wallpaper_astronomy_astrophotography_2538107)!!, "Flowers Gone Crazy", " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley "))
 
         news_articles.setHasFixedSize(true)
-        val manager = StaggeredGridLayoutManager(3, RecyclerView.VERTICAL)
+        val manager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
 //        val manager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         val adapter = NewsListAdapter(newsArticles)
         news_articles.layoutManager = manager
